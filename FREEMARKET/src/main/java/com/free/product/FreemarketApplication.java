b846500689139e5706e57884e886fcb23002ed3c
ezensy <ezensy@DESKTOP-T3C2CH1>
2022-11-24 16:20:30 +0900

    PULL

---
+++ FREEMARKET/src/main/java/com/free/product/FreemarketApplication.java
@@ -0,0 +1,13 @@
+package com.free.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarketApplication.class, args);
+	}
+
+}
---
+++ FREEMARKET/src/test/java/com/free/product/FreemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.free.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
