73161d9738ae245ebcddfdcb19082e026c57116a
나세영 <sktpdud97@naver.com>
2022-12-01 17:35:45 +0900

    20221201_3

--- FREEMARKET/src/main/java/com/free/trade/controller/LoginController.java
+++ FREEMARKET/src/main/java/com/free/trade/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.free.trade.controller;
 
+import com.free.trade.dto.LoginDto;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,15 +8,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Slf4j
 @Controller
 public class LoginController {
     Logger logger = LoggerFactory.getLogger(MainController.class);
-
+    LoginDto loginDto = new LoginDto();
     @GetMapping("/freeMarket/login")
-    public String loginController(Model model){
-
+    public String loginController(HttpSession session){
         logger.info("## loginController 진입 ##");
+        //   session.getAttribute("id",id);
         return "login";
+    }
+    public String loginCheckController(HttpSession session){
+
+        return "redirect:/";
     }
 }
